e2278904d8e5f10b15bf193d604beed470483784
FedericoAmura <federicoamura@gmail.com>
2024-12-03 18:10:27 +0100

    feat: error handling in automation endpoints

--- src/routers/automation.ts
+++ src/routers/automation.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import express, { Response } from 'express';
 
 import { StateMachine } from '@lit-protocol/automation';
 
@@ -6,68 +7,88 @@ const automationRouter = express.Router();
 
 const machines: Record<string, StateMachine> = {};
 
+function handleError(e: unknown, response: Response) {
+  console.error(e);
+  response.status(500).json({
+    error: e,
+  });
+}
+
 automationRouter.post('/automation/create', async (request, response) => {
-  // TODO validate state machine definition
-  console.log(`create machine payload: ${JSON.stringify(request.body)}`);
+  try {
+    // TODO validate state machine definition with zod
+    // TODO accept state machine starting state
+    console.log(`create machine payload: ${JSON.stringify(request.body)}`);
 
-  const {
-    litContractsConfig,
-    litNodeClientConfig,
-    states,
-    transitions,
-  } = request.body;
+    const {
+      litContractsConfig,
+      litNodeClientConfig,
+      states,
+      transitions,
+    } = request.body;
 
-  const stateMachine = StateMachine.fromDefinition({
-    debug: true, // TODO remove
-    litContracts: litContractsConfig,
-    litNodeClient: litNodeClientConfig,
-    states,
-    transitions,
-  });
+    const stateMachine = StateMachine.fromDefinition({
+      debug: true, // TODO remove
+      litContracts: { debug: true, ...litContractsConfig },
+      litNodeClient: { debug: true, ...litNodeClientConfig },
+      states,
+      transitions,
+    });
 
-  if (machines[stateMachine.id]) {
-    // Machine chose an ID that is already in use. Highly unlikely. Force user to retry
-    return response.status(500).json({ error: 'Could not create machine' });
+    if (machines[stateMachine.id]) {
+      // Machine chose an ID that is already in use. Highly unlikely. Force user to retry
+      throw new Error('Server failed to create machine. Try again');
+    }
+
+    machines[stateMachine.id] = stateMachine;
+
+    response.status(200).json({
+      id: stateMachine.id,
+      status: stateMachine.status,
+    });
+  } catch (e) {
+    handleError(e, response);
   }
-
-  machines[stateMachine.id] = stateMachine;
-
-  response.status(200).json({
-    id: stateMachine.id,
-    status: stateMachine.status,
-  });
 });
 
 automationRouter.post('/automation/start/:id/:state', async (request, response) => {
-  const { id, state } = request.params;
-  const machine = machines[id];
+  try {
+    const { id, state } = request.params;
+    const machine = machines[id];
 
-  if (!machine) {
-    return response.status(404).json({ error: 'Machine not found' });
+    if (!machine) {
+      return response.status(404).json({ error: 'Machine not found' });
+    }
+
+    if (machine.isRunning) {
+      return response.status(400).json({ error: 'Machine is already started' });
+    }
+
+    await machine.startMachine(state);
+    response.status(200).json({ id: machine.id, status: machine.status });
+  } catch (e) {
+    handleError(e, response);
   }
-
-  if (machine.isRunning) {
-    return response.status(400).json({ error: 'Machine is already started' });
-  }
-
-  await machine.startMachine(state);
-  response.status(200).json({ id: machine.id, status: machine.status });
 });
 
 automationRouter.post('/automation/stop/:id', async (request, response) => {
-  const { id } = request.params;
-  const machine = machines[id];
+  try {
+    const { id } = request.params;
+    const machine = machines[id];
 
-  if (!machine) {
-    return response.status(404).json({ error: 'Machine not found' });
+    if (!machine) {
+      return response.status(404).json({ error: 'Machine not found' });
+    }
+
+    if (!machine.isRunning) {
+      return response.status(400).json({ error: 'Machine is already stopped' });
+    }
+
+    await machine.stopMachine();
+    response.status(200).json({ id: machine.id, status: machine.status });
+  } catch (e) {
+    handleError(e, response);
   }
-
-  if (!machine.isRunning) {
-    return response.status(400).json({ error: 'Machine is already stopped' });
-  }
-
-  await machine.stopMachine();
-  response.status(200).json({ id: machine.id, status: machine.status });
 });
 
 export default automationRouter;
